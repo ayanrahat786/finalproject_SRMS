e9156cd07cb7aee8122054c93f18bcda0232abe8
memebybunny <86987476+memebybunny@users.noreply.github.com>
2023-04-09 20:20:16 +0530

    Update add-students.php

--- add-students.php
+++ add-students.php
@@ -50,140 +50,155 @@ $error="Something went wrong. Please try again";
 ?>
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    	<meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>SMS Admin| Student Admission< </title>
-        <link rel="stylesheet" href="css/bootstrap.min.css" media="screen" >
-        <link rel="stylesheet" href="css/font-awesome.min.css" media="screen" >
-        <link rel="stylesheet" href="css/animate-css/animate.min.css" media="screen" >
-        <link rel="stylesheet" href="css/lobipanel/lobipanel.min.css" media="screen" >
-        <link rel="stylesheet" href="css/prism/prism.css" media="screen" >
-        <link rel="stylesheet" href="css/select2/select2.min.css" >
-        <link rel="stylesheet" href="css/main.css" media="screen" >
-        <script src="js/modernizr/modernizr.min.js"></script>
-    </head>
-    <body class="top-navbar-fixed">
-        <div class="main-wrapper">
 
-            <!-- ========== TOP NAVBAR ========== -->
-  <?php include('includes/topbar.php');?> 
-            <!-- ========== WRAPPER FOR BOTH SIDEBARS & MAIN CONTENT ========== -->
-            <div class="content-wrapper">
-                <div class="content-container">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>SMS Admin| Student Admission< </title>
+            <link rel="stylesheet" href="css/bootstrap.min.css" media="screen">
+            <link rel="stylesheet" href="css/font-awesome.min.css" media="screen">
+            <link rel="stylesheet" href="css/animate-css/animate.min.css" media="screen">
+            <link rel="stylesheet" href="css/lobipanel/lobipanel.min.css" media="screen">
+            <link rel="stylesheet" href="css/prism/prism.css" media="screen">
+            <link rel="stylesheet" href="css/select2/select2.min.css">
+            <link rel="stylesheet" href="css/main.css" media="screen">
+            <script src="js/modernizr/modernizr.min.js"></script>
+</head>
 
-                    <!-- ========== LEFT SIDEBAR ========== -->
-                   <?php include('includes/leftbar.php');?>  
-                    <!-- /.left-sidebar -->
+<body class="top-navbar-fixed">
+    <div class="main-wrapper">
 
-                    <div class="main-page">
+        <!-- ========== TOP NAVBAR ========== -->
+        <?php include('includes/topbar.php');?>
+        <!-- ========== WRAPPER FOR BOTH SIDEBARS & MAIN CONTENT ========== -->
+        <div class="content-wrapper">
+            <div class="content-container">
 
-                     <div class="container-fluid">
-                            <div class="row page-title-div">
-                                <div class="col-md-6">
-                                    <h2 class="title">Student Admission</h2>
-                                
-                                </div>
-                                
-                                <!-- /.col-md-6 text-right -->
+                <!-- ========== LEFT SIDEBAR ========== -->
+                <?php include('includes/leftbar.php');?>
+                <!-- /.left-sidebar -->
+
+                <div class="main-page">
+
+                    <div class="container-fluid">
+                        <div class="row page-title-div">
+                            <div class="col-md-6">
+                                <h2 class="title">Student Admission</h2>
+
                             </div>
-                            <!-- /.row -->
-                            <div class="row breadcrumb-div">
-                                <div class="col-md-6">
-                                    <ul class="breadcrumb">
-                                        <li><a href="dashboard.php"><i class="fa fa-home"></i> Home</a></li>
-                                
-                                        <li class="active">Student Admission</li>
-                                    </ul>
-                                </div>
-                             
-                            </div>
-                            <!-- /.row -->
+
+                            <!-- /.col-md-6 text-right -->
                         </div>
-                        <div class="container-fluid">
-                           
+                        <!-- /.row -->
+                        <div class="row breadcrumb-div">
+                            <div class="col-md-6">
+                                <ul class="breadcrumb">
+                                    <li><a href="dashboard.php"><i class="fa fa-home"></i> Home</a></li>
+
+                                    <li class="active">Student Admission</li>
+                                </ul>
+                            </div>
+
+                        </div>
+                        <!-- /.row -->
+                    </div>
+                    <div class="container-fluid">
+
                         <div class="row">
-                                    <div class="col-md-12">
-                                        <div class="panel">
-                                            <div class="panel-heading">
-                                                <div class="panel-title">
-                                                    <h5>Fill the Student info</h5>
-                                                </div>
-                                            </div>
-                                            <div class="panel-body">
-<?php if($msg){?>
-<div class="alert alert-success left-icon-alert" role="alert">
- <strong>Well done!</strong><?php echo htmlentities($msg); ?>
- </div><?php } 
+                            <div class="col-md-12">
+                                <div class="panel">
+                                    <div class="panel-heading">
+                                        <div class="panel-title">
+                                            <h5>Fill the Student info</h5>
+                                        </div>
+                                    </div>
+                                    <div class="panel-body">
+                                        <?php if($msg){?>
+                                        <div class="alert alert-success left-icon-alert" role="alert">
+                                            <strong>Well done!</strong><?php echo htmlentities($msg); ?>
+                                        </div><?php } 
 else if($error){?>
-    <div class="alert alert-danger left-icon-alert" role="alert">
+                                        <div class="alert alert-danger left-icon-alert" role="alert">
                                             <strong>Oh snap!</strong> <?php echo htmlentities($error); ?>
                                         </div>
                                         <?php } ?>
-                                                <form class="form-horizontal" method="post">
+                                        <form class="form-horizontal" method="post">
 
 
-<div class="form-group">
-<label for="default" class="col-sm-2 control-label">Student's Name </label>
-<div class="col-sm-10">
-<input type="text" name="fullanme" class="form-control" id="fullanme" required="required" autocomplete="off">
-</div>
-</div>
+                                            <div class="form-group">
+                                                <label for="default" class="col-sm-2 control-label">Student's Name
+                                                </label>
+                                                <div class="col-sm-10">
+                                                    <input type="text" name="fullanme" class="form-control"
+                                                        id="fullanme" required="required" autocomplete="off">
+                                                </div>
+                                            </div>
 
 
-<div class="form-group">
-<label for="default" class="col-sm-2 control-label">Father's Name </label>
-<div class="col-sm-10">
-<input type="text" name="father" class="form-control" id="father" required="required" autocomplete="off">
-</div>
-</div>
+                                            <div class="form-group">
+                                                <label for="default" class="col-sm-2 control-label">Father's Name
+                                                </label>
+                                                <div class="col-sm-10">
+                                                    <input type="text" name="father" class="form-control" id="father"
+                                                        required="required" autocomplete="off">
+                                                </div>
+                                            </div>
 
-<div class="form-group">
-<label for="default" class="col-sm-2 control-label">Mother's Name </label>
-<div class="col-sm-10">
-<input type="text" name="mother" class="form-control" id="mother" required="required" autocomplete="off">
-</div>
-</div>
+                                            <div class="form-group">
+                                                <label for="default" class="col-sm-2 control-label">Mother's Name
+                                                </label>
+                                                <div class="col-sm-10">
+                                                    <input type="text" name="mother" class="form-control" id="mother"
+                                                        required="required" autocomplete="off">
+                                                </div>
+                                            </div>
 
-<div class="form-group">
-<label for="default" class="col-sm-2 control-label">Rool Id</label>
-<div class="col-sm-10">
-<input type="text" name="rollid" class="form-control" id="rollid" maxlength="5" required="required" autocomplete="off">
-</div>
-</div>
+                                            <div class="form-group">
+                                                <label for="default" class="col-sm-2 control-label">Rool Id</label>
+                                                <div class="col-sm-10">
+                                                    <input type="text" name="rollid" class="form-control" id="rollid"
+                                                        maxlength="5" required="required" autocomplete="off">
+                                                </div>
+                                            </div>
 
-<div class="form-group">
-<label for="default" class="col-sm-2 control-label">Email id</label>
-<div class="col-sm-10">
-<input type="email" name="emailid" class="form-control" id="email" required="required" autocomplete="off">
-</div>
-</div>
-
-
-
-<div class="form-group">
-<label for="default" class="col-sm-2 control-label">Gender</label>
-<div class="col-sm-10">
-<input type="radio" name="gender" value="Male" required="required" checked=""> &nbsp;Male <input type="radio" name="gender" value="Female" required="required"> &nbsp;Female <input type="radio" name="gender" value="Other" required="required"> &nbsp;Other
-</div>
-</div>
-
-
+                                            <div class="form-group">
+                                                <label for="default" class="col-sm-2 control-label">Email id</label>
+                                                <div class="col-sm-10">
+                                                    <input type="email" name="emailid" class="form-control" id="email"
+                                                        required="required" autocomplete="off">
+                                                </div>
+                                            </div>
 
 
 
+                                            <div class="form-group">
+                                                <label for="default" class="col-sm-2 control-label">Gender</label>
+                                                <div class="col-sm-10">
+                                                    <input type="radio" name="gender" value="Male" required="required"
+                                                        checked=""> &nbsp;Male <input type="radio" name="gender"
+                                                        value="Female" required="required"> &nbsp;Female <input
+                                                        type="radio" name="gender" value="Other" required="required">
+                                                    &nbsp;Other
+                                                </div>
+                                            </div>
 
 
 
 
 
-<div class="form-group">
-<label for="default" class="col-sm-2 control-label">Class</label>
-<div class="col-sm-10">
- <select name="class" class="form-control" id="default" required="required">
-<option value="">Select Class</option>
-<?php $sql = "SELECT * from tblclasses";
+
+
+
+
+
+                                            <div class="form-group">
+                                                <label for="default" class="col-sm-2 control-label">Class</label>
+                                                <div class="col-sm-10">
+                                                    <select name="class" class="form-control" id="default"
+                                                        required="required">
+                                                        <option value="">Select Class</option>
+                                                        <?php $sql = "SELECT * from tblclasses";
 $query = $dbh->prepare($sql);
 $query->execute();
 $results=$query->fetchAll(PDO::FETCH_OBJ);
@@ -191,46 +206,52 @@ if($query->rowCount() > 0)
 {
 foreach($results as $result)
 {   ?>
-<option value="<?php echo htmlentities($result->id); ?>"><?php echo htmlentities($result->ClassName); ?>&nbsp; Section-<?php echo htmlentities($result->Section); ?></option>
-<?php }} ?>
+                                                        <option value="<?php echo htmlentities($result->id); ?>">
+                                                            <?php echo htmlentities($result->ClassName); ?>&nbsp;
+                                                            Section-<?php echo htmlentities($result->Section); ?>
+                                                        </option>
+                                                        <?php }} ?>
 
- </select>
-                                                        </div>
-                                                    </div>
-                                                    <div class="form-group">
-        <label for="default" class="col-sm-2 control-label">Contact No.</label>
-        <div class="col-sm-10">
-        <input type="tel" name="number" class="form-control" id="number" required="required" autocomplete="off">
-        </div>
-        </div>
-                                                    
-        <div class="form-group">
-            <label for="date" class="col-sm-2 control-label">DOB</label>
-            <div class="col-sm-10">
-                <input type="date"  name="dob" class="form-control" id="date">
-            </div>
-         </div>
-         <div class="form-group">
-        <label for="default" class="col-sm-2 control-label">Address</label>
-        <div class="col-sm-10">
-        <input type="text" name="address" class="form-control" id="address" required="required" autocomplete="off">
-        </div>
-        </div>
-                                                            
-
-                                                    
-                                                    <div class="form-group">
-                                                        <div class="col-sm-offset-2 col-sm-10">
-                                                            <button type="submit" name="submit" class="btn btn-primary">Add</button>
-                                                        </div>
-                                                    </div>
-                                                </form>
-
+                                                    </select>
+                                                </div>
                                             </div>
-                                        </div>
+                                            <div class="form-group">
+                                                <label for="default" class="col-sm-2 control-label">Contact No.</label>
+                                                <div class="col-sm-10">
+                                                    <input type="tel" name="number" class="form-control" id="number"
+                                                        required="required" autocomplete="off">
+                                                </div>
+                                            </div>
+
+                                            <div class="form-group">
+                                                <label for="date" class="col-sm-2 control-label">DOB</label>
+                                                <div class="col-sm-10">
+                                                    <input type="date" name="dob" class="form-control" id="date">
+                                                </div>
+                                            </div>
+                                            <div class="form-group">
+                                                <label for="default" class="col-sm-2 control-label">Address</label>
+                                                <div class="col-sm-10">
+                                                    <input type="text" name="address" class="form-control" id="address"
+                                                        required="required" autocomplete="off">
+                                                </div>
+                                            </div>
+
+
+
+                                            <div class="form-group">
+                                                <div class="col-sm-offset-2 col-sm-10">
+                                                    <button type="submit" name="submit"
+                                                        class="btn btn-primary">Add</button>
+                                                </div>
+                                            </div>
+                                        </form>
+
                                     </div>
-                                    <!-- /.col-md-12 -->
                                 </div>
+                            </div>
+                            <!-- /.col-md-12 -->
+                        </div>
                     </div>
                 </div>
                 <!-- /.content-container -->
@@ -247,16 +268,17 @@ foreach($results as $result)
         <script src="js/select2/select2.min.js"></script>
         <script src="js/main.js"></script>
         <script>
-            $(function($) {
-                $(".js-states").select2();
-                $(".js-states-limit").select2({
-                    maximumSelectionLength: 2
-                });
-                $(".js-states-hide").select2({
-                    minimumResultsForSearch: Infinity
-                });
+        $(function($) {
+            $(".js-states").select2();
+            $(".js-states-limit").select2({
+                maximumSelectionLength: 2
             });
+            $(".js-states-hide").select2({
+                minimumResultsForSearch: Infinity
+            });
+        });
         </script>
-    </body>
+</body>
+
 </html>
 <?PHP } ?>
